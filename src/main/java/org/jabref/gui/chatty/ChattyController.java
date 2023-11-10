5477731deb3920ae7380dd095a38426c28080ae6
Severin Memmishofer <severin.memmishofer@gmx.ch>
2023-11-10 17:26:08 +0100

    Made Basic Chat GUI for the Prototype

--- src/main/java/org/jabref/gui/chatty/ChattyController.java
+++ src/main/java/org/jabref/gui/chatty/ChattyController.java
@@ -1,4 +1,21 @@
 package org.jabref.gui.chatty;
 
+import java.awt.Button;
+
+import javafx.fxml.FXML;
+
 public class ChattyController {
+
+    @FXML
+    private Button sendButton;
+    @FXML
+    private Button copyToClipboard;
+
+    public void send() {
+        System.out.println("Sends Chat to Chatty");
+    }
+
+    public void copyToClipboard() {
+        System.out.println("Copies last message to Clipboard");
+    }
 }
