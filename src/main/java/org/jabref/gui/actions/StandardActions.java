c11563545810e637ec55043ab1c0c5323919dbc1
SoulKindred <michirrappi@gmail.com>
2023-12-11 18:04:16 +0100

    Set new icon for setApiKey function

--- src/main/java/org/jabref/gui/actions/StandardActions.java
+++ src/main/java/org/jabref/gui/actions/StandardActions.java
@@ -27,7 +27,7 @@ public enum StandardActions implements Action {
     DELETE_ENTRY(Localization.lang("Delete entry"), IconTheme.JabRefIcons.DELETE_ENTRY, KeyBinding.DELETE_ENTRY),
     SEND_AS_EMAIL(Localization.lang("Send as email"), IconTheme.JabRefIcons.EMAIL),
     REBUILD_FULLTEXT_SEARCH_INDEX(Localization.lang("Rebuild fulltext search index"), IconTheme.JabRefIcons.FILE),
-    SET_API_KEY(Localization.lang("Set API key"), IconTheme.JabRefIcons.FILE),
+    SET_API_KEY(Localization.lang("Set API key"), IconTheme.JabRefIcons.CHATTY),
     OPEN_EXTERNAL_FILE(Localization.lang("Open file"), IconTheme.JabRefIcons.FILE, KeyBinding.OPEN_FILE),
     OPEN_URL(Localization.lang("Open URL or DOI"), IconTheme.JabRefIcons.WWW, KeyBinding.OPEN_URL_OR_DOI),
     SEARCH_SHORTSCIENCE(Localization.lang("Search ShortScience")),
