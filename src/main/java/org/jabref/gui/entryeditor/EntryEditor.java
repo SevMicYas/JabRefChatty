099c2368edac9a3e89b4ba96f0a64ff4fc593143
YashTrivedi7 <yash@hispeed.ch>
2023-12-11 16:54:05 +0100

    Changes by severinmemmishofer

--- src/main/java/org/jabref/gui/entryeditor/EntryEditor.java
+++ src/main/java/org/jabref/gui/entryeditor/EntryEditor.java
@@ -56,6 +56,7 @@ import org.jabref.model.database.BibDatabaseContext;
 import org.jabref.model.entry.BibEntry;
 import org.jabref.model.entry.BibEntryTypesManager;
 import org.jabref.model.entry.field.Field;
+import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.util.FileUpdateMonitor;
 import org.jabref.preferences.PreferencesService;
 
